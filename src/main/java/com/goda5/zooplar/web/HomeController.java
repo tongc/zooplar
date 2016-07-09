f86e0af0cac934e232204e360cc2bb2cc898ebcb
chentongtc@gmail.com <chentongtc@gmail.com>
2016-07-09 22:12:43 +0100

    added caching

--- src/main/java/com/goda5/zooplar/web/HomeController.java
+++ src/main/java/com/goda5/zooplar/web/HomeController.java
@@ -1,5 +1,8 @@
 package com.goda5.zooplar.web;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.apache.http.client.fluent.Request;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class HomeController {
@@ -18,9 +23,23 @@ public class HomeController {
     @Value("${app.api.datatypes.areaprice}")
     private String areaPrice;
 
+    LoadingCache<String, String> areaPriceData = CacheBuilder.newBuilder()
+            .maximumSize(1000)
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .build(
+                    new CacheLoader<String, String>() {
+                        public String load(String key) throws IOException {
+                            return queryAreaPrice(key);
+                        }
+                    });
+
     @RequestMapping(value = "/areaPrice", produces = "application/xml")
     @ResponseBody
-    private String areaPrice(@RequestParam String postcode) throws IOException {
+    private String areaPrice(@RequestParam String postcode) throws IOException, ExecutionException {
+        return areaPriceData.get(postcode);
+    }
+
+    private String queryAreaPrice(String postcode) throws IOException {
         return Request.Get(url + areaPrice + "?api_key=" + key + "&area=" + postcode + "&output_type=outcode")
                 .connectTimeout(1000)
                 .socketTimeout(1000)
