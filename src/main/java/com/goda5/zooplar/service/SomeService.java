0ce2efd28b48ce6132653c4ec24e2c90d695090c
chentongtc@gmail.com <chentongtc@gmail.com>
2016-10-10 22:04:13 +0100

    play with thread info.

--- src/main/java/com/goda5/zooplar/service/SomeService.java
+++ src/main/java/com/goda5/zooplar/service/SomeService.java
@@ -3,15 +3,27 @@ package com.goda5.zooplar.service;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.management.RuntimeMBeanException;
 
 @Service
 public class SomeService {
     public SomeService() {
         System.out.println("here" + Thread.currentThread().getName());
+        new Thread(() -> {
+            Thread.currentThread().setName("new Thread Tong");
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            throw new RuntimeException();
+        }).start();
     }
 
     @PostConstruct
-    public void dosth() {
+    public void dosth() throws InterruptedException {
         System.out.println("here1" + Thread.currentThread().getName());
+        Thread.sleep(10000);
     }
 }
